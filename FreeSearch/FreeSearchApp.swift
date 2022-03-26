7067320bc70eb92c1b036787409720d3a3042e82
Jaime Capponi Zerene <jaime.capponi.z@gmail.com>
2022-03-26 15:53:34 -0300

    Initial Commit

---
+++ FreeSearch/FreeSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  FreeSearchApp.swift
+//  FreeSearch
+//
+//  Created by Jaime Capponi Zerene on 26-03-22.
+//
+
+import SwiftUI
+
+@main
+struct FreeSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
